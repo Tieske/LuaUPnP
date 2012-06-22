d47477caf1bc6ec5d9b2dc274b9d799d5ed2725e
Thijs Schreijer <thijs@thijsschreijer.nl>
2012-06-23 00:51:50 +0200

    Rewrote most of the test code.

--- LuaUPnP/UPnPtest.lua
+++ LuaUPnP/UPnPtest.lua
@@ -2,82 +2,99 @@
 --  Test module, initialize here, main test function below
 -----------------------------------------------------------------
 
-local myxml = [[<?xml version="1.0" encoding="utf-8"?>
-<root xmlns="urn:schemas-upnp-org:device-1-0">
-   <specVersion>
-      <major>1</major>
-      <minor>0</minor>
-   </specVersion>
-   <device>
-      <deviceType>urn:schemas-upnp-org:device:basic:1</deviceType>
-      <presentationURL>WebLink/87ce90b5-8ce9-4743-a15a-6a28fa40feb5.html</presentationURL>
-      <friendlyName>DD-WRT WLAN router</friendlyName>
-      <manufacturer>Tieske</manufacturer>
-      <manufacturerURL>http://www.thijsschreijer.nl</manufacturerURL>
-      <modelDescription>Network based URL shortcut available to all users on the network (subnet)</modelDescription>
-      <modelName>UPnP WebLink</modelName>
-      <modelNumber />
-      <modelURL>http://www.thijsschreijer.nl/</modelURL>
-      <serialNumber />
-      <UDN>uuid:87ce90b5-8ce9-4743-a15a-6a28fa40feb5</UDN>
-      <iconList>
-         <icon>
-            <mimetype>image/png</mimetype>
-            <width>32</width>
-            <height>32</height>
-            <depth>32</depth>
-            <url>/icon.png</url>
-         </icon>
-         <icon>
-            <mimetype>image/jpg</mimetype>
-            <width>32</width>
-            <height>32</height>
-            <depth>32</depth>
-            <url>/icon.jpg</url>
-         </icon>
-      </iconList>
-   </device>
-</root>
-]]
-
+local copas = require('copas.timer')        -- load Copas socket scheduler
+local dss = require('dss')      -- load darksidesync module
 local upnp = require("LuaUPnP")
-local ixml = upnp.ixml
+local ft = require ("pl.pretty").write
+table.print = function(t) print(ft(t)) end
 
+-- the darksidesync lib has a Lua side piece of code that listens to the UDP signal whenever
+-- a background lib has delivered something.
+-- It provides a socket we need to listen on, and a callback that needs to be called when
+-- the socket is ready to read data. All we need to do is add them to our socket scheduler.
+-- We're using Copas as a socket scheduler, so add the darksidesync socket and the handler in a
+-- Copas way to the scheduler
+copas.addserver(dss.getsocket(), function(skt)
+        skt = copas.wrap(skt)
+        local hdlr = dss.gethandler()
+        while true do
+            hdlr(skt)
+        end
+    end)
 
-local function printtree(n, level)
-	level = level or ""
-	print(level .. tostring(n), n:getNodeName(), n:getNodeValue(), n:hasChildNodes())
-	for node in n:children() do
-		printtree(node, level .. "   ")
-	end
-end
-
------------------------------------------------------------------
---  Test function, put main code here
------------------------------------------------------------------
-
-local test = function()
-	print (myxml)
-	print ("\n\nPress enter to continue...")
+local wait = function(t)
+	print ("\n\n" .. tostring(t or "Press enter to continue..."))
 	io.read()
-
-	print(type(myxml))
-	local file = ixml.ParseBuffer(myxml)
-
-	printtree(file)
-
 end
+
+local errf = function(msg)
+	print (debug.traceback(msg or "Stacktrace:"))
+    copas.exitloop()
+end
+
+
+-----------------------------------------------------------------
+--  Test functions, put main code here.
+-----------------------------------------------------------------
+local cp        -- controlpoint id
+
+local testlist = {
+    function()
+        print("List of the UPnP module:\n========================\n")
+        table.print(upnp);
+        print("\n\n")
+    end,
+
+    function()
+        cp = upnp.RegisterClient()
+        print(tostring(cp))
+    end,
+
+    function()
+        print("starting UPnP")
+        upnp.Init()
+    end,
+
+
+
+    function()
+        print("stopping UPnP")
+        upnp.Finish()
+    end,
+
+}
+
 
 -----------------------------------------------------------------
 --  Generic test functionality to start and trace errors
 -----------------------------------------------------------------
 
+local timer
+local testcount = 1
+-- test function to run tests in a row
+local test = function()
 
-local errf = function(msg)
-	print (debug.traceback(msg or "Stacktrace:"))
+    if testlist[testcount] then
+        -- run next test
+        print ("=========== starting test " .. testcount .. " ===========")
+        xpcall(testlist[testcount], errf)
+        testcount = testcount + 1
+    else
+        -- we're done, exit
+        print ("=========== tests completed ===========")
+        timer:cancel()
+        copas.exitloop()
+    end
+
 end
 
-xpcall(test, errf)
-print ("Press enter to exit...")
-io.read()
+
+wait ("Press enter to start...")
+
+-- create timer for test function, every 0.5 second
+timer = copas.newtimer(test, test, nil, true, nil):arm(1)
+
+copas.loop()
+
+--wait ("Press enter to exit...")
 
