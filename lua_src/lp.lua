4ec9f1da594b4e254e2744e99c5fa8127d18f8cb
Thijs Schreijer <thijs@thijsschreijer.nl>
2012-10-02 12:38:47 +0200

    updated comments/documentation

--- lua_src/lp.lua
+++ lua_src/lp.lua
@@ -11,10 +11,18 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
 ]]
 
+---------------------------------------------------------------------
+-- Lua Pages template engine. Code was taken from LuaDoc and adapted
+-- for LuaUPnP.
+-- @class module
+-- @name upnp.lp
+-- @copyright 2004-2007 The Kepler Project, LuaUPnP modifications 2012 <a href="http://www.thijsschreijer.nl">Thijs Schreijer</a>, <a href="http://github.com/Tieske/LuaUPnP">LuaUPnP</a>
+-- @release Version 0.x, LuaUPnP.
+
 
 local find, format, gsub, strsub = string.find, string.format, string.gsub, string.sub
 
-local M = {}
+local lp = {}
 
 ----------------------------------------------------------------------------
 -- function to do output
@@ -41,9 +49,9 @@ end
 ----------------------------------------------------------------------------
 -- Translate the template to Lua code.
 -- @param s String to translate.
--- @return String with translated code.
+-- @return String with translated, but not compiled, code.
 ----------------------------------------------------------------------------
-function M.translate (s)
+function lp.translate (s)
 	s = gsub(s, "<%%(.-)%%>", "<?lua %1 ?>")
 	local res = {}
 	local start = 1   -- start of untranslated part in `s'
@@ -71,9 +79,9 @@ end
 ----------------------------------------------------------------------------
 -- Defines the name of the output function.
 -- @param f String with the name of the function which produces output. Default
--- value is "return".
+-- value is <code>"return"</code>.
 
-function M.setoutfunc (f)
+function lp.setoutfunc (f)
 	outfunc = f
 end
 
@@ -102,14 +110,14 @@ end
 local cache = {}
 
 ----------------------------------------------------------------------------
--- Translates a template into a Lua function.
+-- Translates a template into a compiled Lua function.
 -- Does NOT execute the resulting function.
--- @param string String with the template to be translated.
+-- @param str String with the template to be translated.
 -- @param chunkname String with the name of the chunk, for debugging purposes.
 -- @return Function with the resulting translation.
 
-function M.compile (str, chunkname)
-	local f, err = loadstring (M.translate (str), chunkname)
+function lp.compile (str, chunkname)
+	local f, err = loadstring (lp.translate (str), chunkname)
 	if not f then error (err, 3) end
 	return f
 end
@@ -123,10 +131,10 @@ end
 -- @param env Table with the environment to run the resulting function.
 -- If <code>nil</code> then a new environment will be used, both the new and
 -- the provided environment will be equipped with the basic Lua functions.
--- @return the results of the function set by lp.setoutfunc(), but might also
+-- @return the results of the function set by <code>lp.setoutfunc()</code>, but might also
 -- throw an error
 
-function M.includefile (filename, env)
+function lp.includefile (filename, env)
   local prog = cache[filename]
   if not prog then
     -- read the whole contents of the file
@@ -134,7 +142,7 @@ function M.includefile (filename, env)
     local src = fh:read("*a")
     fh:close()
     -- translates the file into a function
-    prog = M.compile (src, '@'..filename)
+    prog = lp.compile (src, '@'..filename)
     cache[filename] = prog
   end
 
@@ -154,39 +162,39 @@ end
 
 ----------------------------------------------------------------------------
 -- Translates and executes a template in a given 'module'. It will search 
--- for a file located in the module path. It will look for a '.upnp' extension
+-- for a file located in the module path. It will look for a <code>'.upnp'</code> extension
 -- The translation creates a Lua function which will be executed in an
--- optionally given environment (will call the includefile() function).
+-- optionally given environment (will call the <code>lp.includefile()</code> function).
 -- @param template String with the name of the module containing the template.
 -- @param env Table with the environment to run the resulting function.
--- @return the results of the function set by lp.setoutfunc(), but might also
+-- @return the results of the function set by <code>lp.setoutfunc()</code>, but might also
 -- throw an error
 
-function M.includemodule(template, env)
+function lp.includemodule(template, env)
 	-- search using package.path (modified to search .lp instead of .lua
 	local search_path = string.gsub(package.path, "%.lua", "%.upnp")
 	local templatepath = search(search_path, template)
 	assert(templatepath, string.format("template `%s' not found", template))
 
-	return M.includefile(templatepath, env)
+	return lp.includefile(templatepath, env)
 end
 
 ----------------------------------------------------------------------------
 -- Translates and executes a template in a given 'module' or file. This method
 -- enables to call directly on the module table. It will first call 
--- lp.includefile(), and if that fails lp.includemodule()
+-- <code>lp.includefile()</code>, and if that fails <code>lp.includemodule()</code>
 -- @name __call
 -- @class function
 -- @param name the file or module name.
 -- @param env Table with the environment to run the resulting function.
 -- @return the results or <code>nil + errormsg</code>
-setmetatable(M, { __call = function(self, name, env)
-    local success, result = pcall(self.includefile, name, env)
+setmetatable(lp, { __call = function(self, name, env)
+    local success, result = pcall(lp.includefile, name, env)
     if not success then
-      success, result = self.includemodule(self.includemodule, name, env)
+      success, result = self.includemodule(lp.includemodule, name, env)
     end
     if success then return result end
     return nil, result  -- it's an error
   end})
 
-return M
+return lp
